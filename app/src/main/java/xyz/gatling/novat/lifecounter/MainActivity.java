098a5ac826290f5afa896a4818f27cb04f8ff854
kentarosu <gimmiepepsi@gmail.com>
2016-03-14 22:15:19 -0400

    Keeps the app full screen between orientation shifts.

--- app/src/main/java/xyz/gatling/novat/lifecounter/MainActivity.java
+++ app/src/main/java/xyz/gatling/novat/lifecounter/MainActivity.java
@@ -43,6 +43,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onResume() {
         super.onResume();
+        onWindowFocusChanged(true);
     }
 
     private void resetLifePools(){
